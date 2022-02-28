d9de81aa140e815cbf1dd2073423aa208b8b1293
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-02-28 09:04:59 +0300

    update views

--- album/views.py
+++ album/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
+from album.models import Image,Category,Location
+
 # Create your views here.
 def index(request):
-  return render(request,'index.html')
+  images = Image.objects.all()
+  return render(request,'index.html',{"images":images,})
+
+def location(request):
+  if location == 'nairobi':
+    return redirect('nairobi.html')
+
+  return render(request,'diaspora.html')    
+
